94e9aab1fe5b44ef4009a2fd72e299ba7a8c40f7
MohamedEmbrator <128060291+MohamedEmbrator@users.noreply.github.com>
2023-07-23 01:59:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let allNavDivs = document.querySelectorAll("nav > div > div");
+let globalBooks = document.querySelector(".global-books");
+let navigatin = document.getElementById("header");
+allNavDivs.forEach(function (ele) {
+  ele.onclick = function () {
+    // Remove Active Class From All Elements
+    allNavDivs.forEach(function (ele) {
+      ele.classList.remove("active");
+    });
+
+    // Add Active Class To This Element
+    this.classList.add("active");
+    let activeDiv = document.querySelector(
+      "header .container nav > div > .active"
+    );
+    activeDiv.addEventListener("click", function () {
+      activeDiv.classList.toggle("active");
+    });
+  };
+});
+window.onscroll = function () {
+  if (window.scrollY >= 150) {
+    navigatin.classList.add("fixed");
+  } else {
+    navigatin.classList.remove("fixed");
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+let toggleBtn = document.getElementById("btn");
+let navBar = document.getElementById("nav");
+toggleBtn.addEventListener("click", function () {
+  navBar.classList.toggle("show");
+});
