5233ae60b8dadc57b309abf4704de19c276d6c10
MohamedEmbrator <128060291+MohamedEmbrator@users.noreply.github.com>
2023-07-29 21:23:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -45,27 +45,47 @@ let selection = document.querySelector("header.header .container > select");
 let landingTitle = document.querySelector(
   ".landing .container .content .text h1"
 );
+let logo = document.querySelector("header.header .container .logo > span");
+let schoolBooksHome = document.querySelector(
+  "header.header .container > nav.nav > .global > div > span"
+);
+let ForeignBooks = document.querySelector(
+  "header.header .container > nav.nav > .out > div > span"
+);
+
 selection.onchange = function () {
   window.localStorage.setItem("lang", selection.value);
   if (window.localStorage.getItem("lang") === "en") {
     selection.value = window.localStorage.getItem("lang");
+    document.body.style.direction = "ltr";
     document.title = "Outstanding Platform | Home Page";
     landingTitle.innerHTML = "Welcome To The Outstanding Platform";
+    logo.innerHTML = "Outstanding Platform";
+    schoolBooksHome.innerHTML = "School Books";
   } else if (window.localStorage.getItem("lang") === "ar") {
     selection.value = window.localStorage.getItem("lang");
+    document.body.style.direction = "rtl";
     document.title = "منصة المتفوقين | الصفحة الرئيسية";
     landingTitle.innerHTML = "مرحباً بكم في منصة المتفوقين";
+    logo.innerHTML = "منصة المتفوقين";
+    schoolBooksHome.innerHTML = "الكتب المدرسية";
   }
 };
 //  ------- -- ------- - ------- ------ ----- ------- ------
 if (window.localStorage.getItem("lang")) {
   selection.value = window.localStorage.getItem("lang");
   if (window.localStorage.getItem("lang") === "en") {
+    document.body.style.direction = "ltr";
     document.title = "Outstanding Platform | Home Page";
     landingTitle.innerHTML = "Welcome To The Outstanding Platform";
+    logo.innerHTML = "Outstanding Platform";
+    schoolBooksHome.innerHTML = "School Books";
   } else if (window.localStorage.getItem("lang") === "ar") {
+    document.body.style.direction = "rtl";
     document.title = "منصة المتفوقين | الصفحة الرئيسية";
     landingTitle.innerHTML = "مرحباً بكم في منصة المتفوقين";
+    logo.innerHTML = "منصة المتفوقين";
+    schoolBooksHome.innerHTML = "الكتب المدرسية";
   }
 }
 // End English And Arabic Switch
