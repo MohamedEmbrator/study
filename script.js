6040892252b3818728d6ae5c7ff18dfb59c31f4d
MohamedEmbrator <128060291+MohamedEmbrator@users.noreply.github.com>
2023-11-04 18:48:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let allNavDivs = document.querySelectorAll("nav > div > div");
 let globalBooks = document.querySelector(".global-books");
 let navigatin = document.getElementById("header");
+const scrollButton = document.querySelector("button.scroll");
+const scrollProgress = document.querySelector(".scroll-height");
+
 allNavDivs.forEach(function (ele) {
   ele.onclick = function () {
     // Remove Active Class From All Elements
@@ -23,7 +26,7 @@ btn.onclick = function () {
   window.scrollTo({
     left: 0,
     top: 0,
-    behavior: "smooth",
+    behavior: "smooth"
   });
 };
 
@@ -32,10 +35,46 @@ let navBar = document.getElementById("nav");
 toggleBtn.addEventListener("click", function () {
   navBar.classList.toggle("show");
 });
+btn.addEventListener("click", function () {
+  navigationBar.classList.toggle("show");
+});
+const totalHeight = document.body.scrollHeight - window.innerHeight;
+const scrollProgressDiv = document.querySelector(".scroll-height > div");
 window.onscroll = function () {
   if (window.scrollY >= 150) {
     navigatin.classList.add("fixed");
   } else {
     navigatin.classList.remove("fixed");
   }
+  if (window.scrollY >= 600) {
+    if (window.innerWidth <= 450) {
+      scrollButton.style.display = "none";
+    } else {
+      scrollButton.style.display = "flex";
+    }
+  } else {
+    scrollButton.style.display = "none";
+  }
+  const progressHeight = (window.pageYOffset / totalHeight) * 100;
+  if (window.scrollY <= 0) {
+    scrollProgress.style.display = "none";
+  } else {
+    scrollProgress.style.display = "block";
+  }
+  scrollProgressDiv.style.width = `${progressHeight}%`;
 };
+scrollProgress.style.top = `${
+  document.querySelector("header.header").offsetHeight
+}px`;
+window.onresize = function () {
+  scrollProgress.style.top = `${
+    document.querySelector("header.header").offsetHeight
+  }px`;
+};
+scrollButton.addEventListener("click", function () {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth"
+  });
+});
